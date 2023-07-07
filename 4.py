2b3997484c711a0c305ae56de8ff4818461112f6
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-08 00:17:28 +0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,16 @@
 #Implement enumerate function
 
+# x = ['a', 'b', 'c']
+# z = dict(enumerate(x,97))
+# print(z)
+
+
+
 x = ['a', 'b', 'c']
-z = dict(enumerate(x,97))
-print(z)
+y = 97
+res = []
+for i in range(len(x)):
+    t = (y,x[i]) 
+    res.append(t)
+print(res)
     
