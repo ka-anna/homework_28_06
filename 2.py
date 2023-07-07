40b2a612afdf8daa61363930700a5765e7d27712
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-08 00:05:22 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,16 @@
 #Implement zip function
 
+# x = [1,2,3,4]
+# y = [11,12,13]
+
+# z = list(zip(x,y))
+# print(z)
+
+
 x = [1,2,3,4]
 y = [11,12,13]
-
-z = list(zip(x,y))
-print(z)
+res = []
+for i in range (len(x)-1):
+    t1 = (x[i], y[i])
+    res.append(t1)
+print(res)
