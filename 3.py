c0ad3fda1a944102a09fda66f2c85c81e99c32a2
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-08 00:10:37 +0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,13 @@
 #Implement filter function
 
+# price = [47, 56, 10, 45, 115, 17, 7, 89]
+# min_p = filter(lambda i: i >20, price)
+# print (list(min_p))
+
+
 price = [47, 56, 10, 45, 115, 17, 7, 89]
-min_p = filter(lambda i: i >20, price)
-print (list(min_p))
+min_p = []
+for i in range(len(price)):
+    if price[i] > 20:
+        min_p.append(price[i])
+print(min_p)
