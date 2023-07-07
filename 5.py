d5b8cdef628dafd2f5726383ecba622184743c2f
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-08 00:23:39 +0400

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,10 @@
 #Implement map function
 
+# price = [47, 56, 10, 45, 115, 17, 7, 89]
+# min_p = map(lambda i: i >20, price)
+# print (list(min_p))
+
 price = [47, 56, 10, 45, 115, 17, 7, 89]
-min_p = map(lambda i: i >20, price)
-print (list(min_p))
+for i in range(len(price)):
+    price[i] *=2
+print(price)
